14d32893ffc3a5f4732627985d5a7fc0d6993da3
Wassim Chegham <github@wassim.dev>
2021-08-03 16:25:44 +0200

    chore: initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,191 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
+    
+    <mat-card *ngIf="!isScanning" class="mat-elevation-z1" [style.backgroundColor]="cards[0].color">
+      <mat-card-header>
+        <mat-card-title>{{ cards[0].title }}</mat-card-title>
+      </mat-card-header>
+      <img mat-card-image src="{{ cards[0].image }}">
+      <mat-card-actions>
+        <button mat-button color="secondary" (click)="start()" >PLAY</button>
+      </mat-card-actions>
+    </mat-card>
+    
+
+    <ul *ngIf="isScanning">
+      <li *ngFor="let card of cards">
+        
+        <mat-card [attr.id]="'card-' + card.id" class="mat-elevation-z1" [style.backgroundColor]="card.color">
+          <mat-card-header>
+            <mat-card-title>{{ card.title }}</mat-card-title>
+          </mat-card-header>
+          <img mat-card-image src="{{ card.image }}">
+          <mat-card-actions>
+            <button mat-button color="primary" (click)="save(card.id)" >SAVE</button>
+            <button mat-button color="secondary" (click)="player.play()" >PLAY</button>
+          </mat-card-actions>
+        </mat-card>
+
+        <audio [attr.id]="'audio-'+card.id" src="{{ card.audio }}" #player></audio>
+        
       </li>
     </ul>
-    
   `,
-  styles: []
+  styles: [`
+    :host {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding-top: 100px;
+    }
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      flex-wrap: wrap;
+    }
+    li {
+      flex: 1;
+      padding: 10px;
+      display: flex;
+      justify-content: center;
+    }
+    mat-card {
+      width: 300px
+    }
+  `]
 })
 export class AppComponent {
-  title = 'nfc-naya';
+  cards = [{
+    title: 'Giraffe',
+    image: '/assets/images/giraffe.jpg',
+    id: 'GIRAFFE',
+    audio: '/assets/audio/giraffe.mp3',
+    color: "#ffffff"
+  }, {
+    title: 'Mouse',
+    image: '/assets/images/mouse.jpg',
+    id: 'MOUSE',
+    audio: '/assets/audio/mouse.mp3',
+    color: "#ffffff"
+  }, {
+    title: 'Squirrel',
+    image: '/assets/images/squirrel.jpg',
+    id: 'SQUIRREL',
+    audio: '/assets/audio/squirrel.mp3',
+    color: "#ffffff"
+  }, {
+    title: 'Bee',
+    image: '/assets/images/bee.jpg',
+    id: 'BEE',
+    audio: '/assets/audio/bee.mp3',
+    color: "#ffffff"
+  }, {
+    title: 'Monkey',
+    image: '/assets/images/monkey.jpg',
+    id: 'MONKEY',
+    audio: '/assets/audio/monkey.mp3',
+    color: "#ffffff"
+  }, {
+    title: 'Cat',
+    image: '/assets/images/cat.jpg',
+    id: 'CAT',
+    audio: '/assets/audio/cat.mp3',
+    color: "#ffffff"
+  }, {
+    title: 'Rabbit',
+    image: '/assets/images/rabbit.jpg',
+    id: 'RABBIT',
+    audio: '/assets/audio/rabbit.mp3',
+    color: "#ffffff"
+  }, {
+    title: 'Panda',
+    image: '/assets/images/panda.jpg',
+    id: 'PANDA',
+    audio: '/assets/audio/panda.mp3',
+    color: "#ffffff"
+  }, {
+    title: 'Bear',
+    image: '/assets/images/bear.jpg',
+    id: 'BEAR',
+    audio: '/assets/audio/bear.mp3',
+    color: "#ffffff"
+  }, {
+    title: 'Dog',
+    image: '/assets/images/dog.jpg',
+    id: 'DOG',
+    audio: '/assets/audio/dog.mp3',
+    color: "#ffffff"
+  }];
+
+  isScanning = false;
+
+  constructor(private snackBar: MatSnackBar) { }
+
+  async start() {
+    try {
+      const ndef = new (window as any).NDEFReader();
+      this.isScanning = true;
+      await ndef.scan();
+      this.notify("Scanning...");
+
+      ndef.addEventListener("readingerror", () => {
+        this.notify(`Error reading tag`);
+      });
+
+      ndef.addEventListener("reading", ({ message, serialNumber }: { message: any, serialNumber: string }) => {
+
+        for (const record of message.records) {
+          this.notify("Record id:    " + record.id);
+          switch (record.recordType) {
+            case "text":
+              const textDecoder = new TextDecoder(record.encoding);
+              this.play(textDecoder.decode(record.data));
+              this.notify(`Scanned: ${textDecoder.decode(record.data)}`);
+              break;
+            case "url":
+              // TODO: Read URL record with record data.
+              break;
+            default:
+            // TODO: Handle other records with record data.
+          }
+        }
+        this.play('cat');
+      });
+    } catch (error) {
+      this.notify(error);
+      this.isScanning = false;
+    }
+  }
+
+  notify(message: string) {
+    this.snackBar.open(message, 'OK');
+  }
+
+  async save(id: string) {
+    try {
+      const ndef = new (window as any).NDEFReader();
+      await ndef.write({
+        records: [{ recordType: "text", data: id }]
+      })
+      this.notify(`${id} saved!`);
+    } catch (error) {
+      this.notify(error);
+    }
+  }
+
+  play(id: string) {
+    const player = document.querySelector(`#audio-${id}`) as HTMLAudioElement;
+    const card = document.querySelector(`#card-${id}`) as HTMLElement;
+    card.classList.add('mat-elevation-z8');
+    card?.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+    player.play();
+    player.onended = () => card.classList.remove('mat-elevation-z8');
+    this.notify(id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const MAT = [
+  MatCardModule,
+  MatButtonModule,
+  MatSnackBarModule
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ...MAT
   ],
   providers: [],
   bootstrap: [AppComponent]
